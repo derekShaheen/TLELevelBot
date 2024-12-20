17c562d645070d0392eea901af518de9a8e0592c
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2024-12-19 22:54:27 -0600

    Rollback

--- configManager.py
+++ configManager.py
@@ -34,8 +34,6 @@ def load_all_user_data(guild_id):
 def save_user_data(guild_id, user_id, data):
     with open(f'data/{guild_id}/{user_id}.yaml', 'w') as file:
         safe_dump(data, file)
-        file.flush()
-        os.fsync(file.fileno())
 
 def load_guild_data(guild_id):
     # Load the guild's data if it exists, otherwise create a new data file
@@ -56,8 +54,6 @@ def load_guild_data(guild_id):
         # Create guild_data.yaml
         with open(f'data/{guild_id}/guild_data.yaml', 'w') as file:
             safe_dump(data, file)
-            file.flush()
-            os.fsync(file.fileno())
         return data
 
 
@@ -65,8 +61,6 @@ def save_guild_data(guild_id, data):
     data = {k: data[k] for k in sorted(data)} # Sort the data before saving
     with open(f'data/{guild_id}/guild_data.yaml', 'w') as file:
         safe_dump(data, file)
-        file.flush()
-        os.fsync(file.fileno())
 
 def load_config():
     default_config = {
